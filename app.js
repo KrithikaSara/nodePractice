a6d06390a55a6dfc907aeb22a982bd060241779c
Krithika <krithisar@gmail.com>
2019-03-29 09:01:04 +0530

    next routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 // const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+// const pug = require('pug');
 
 const app = express();
 const port=process.env.PORT || 3000;
@@ -10,11 +11,11 @@ const port=process.env.PORT || 3000;
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public/')));
-app.set('views', '.src/views');
-app.set('view engine', 'pug');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {list:['a','b'], title:'Library'});
 })
 
 app.listen(port, () => {
